4bd058726136c5bc154c3b5ef5433d34d35c5a7d
shahzaibsalem <120157156+shahzaibsalem@users.noreply.github.com>
2023-05-09 23:43:55 +0500

    Add files via upload

---
+++ Week 2 PD/application/Program.cs
@@ -0,0 +1,299 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using EZInput;
+
+namespace application
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int add=0;
+            int deducted=0;
+            bool login = false;
+            string n;
+            string p;
+            int Catch;
+            string path = "C:\\OOP week2\\Week 2 PD\\record.txt";
+            string path1 = "C:\\OOP week2\\Week 2 PD\\user.txt";
+            List<users> s = new List<users>();
+            List<functionality> s1 = new List<functionality>();
+            loadDataFromFile(path, s);
+            do
+            {
+                loadDataFromFile(path, s);
+                Console.Clear();
+                Catch = menu();
+                if (Catch == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("Enter new username!!!!");
+                    n = Console.ReadLine();
+                    Console.WriteLine("Enter new password!!!!");
+                    p = Console.ReadLine();
+                    bool flag;
+                    flag = validuser(n, p, s);
+                    if(flag==true)
+                    {
+                        Console.WriteLine("User already exists!!!!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Suuccess!!!!");
+                        storeDatatoFile(path, n, p);
+                    }
+                    Console.ReadKey();
+                }
+                else if (Catch == 2)
+                {
+                    bool flag;
+                    Console.Clear();
+                    Console.WriteLine("Enter  username!!!!");
+                    n = Console.ReadLine();
+                    Console.WriteLine("Enter  password!!!!");
+                    p = Console.ReadLine();
+                    flag = validuser(n, p, s);
+                    if (flag == false)
+                    {
+                        Console.WriteLine("Error wrong input!!!!");
+                    }
+                    else if (flag == true)
+                    {
+                        Console.WriteLine("Suuccess!!!!");
+                        login = true;
+                        break;
+                    }
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.Clear();
+                    break;
+                }
+            } while (true);
+            if(login==true)
+            {
+                Console.Clear();
+                int choice;
+                Console.Clear();
+                do
+                {
+                    Console.Clear();
+                    loadUserData(path1,s1);
+                    choice = userMenuBar();
+                    Console.Clear();
+                    if (choice == 1)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("How much money you want to add?");
+                        add = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Added successfully!!!!");
+                        storeUserData(path1, add, deducted);
+                        Console.ReadKey();
+                    }
+                    else if (choice == 2)
+                    {
+                        Console.Clear();
+                        Console.WriteLine("How much money you want to deducted?");
+                        deducted = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Deducted successfully!!!!");
+                        storeUserData(path1, add, deducted);
+                        Console.ReadKey();
+                    }
+                    else if (choice == 3)
+                    {
+                        Console.Clear();
+                        string n1;
+                        Console.WriteLine("Enter old username!!!!");
+                        n1 = Console.ReadLine();
+                        for (int x = 0; x < s.Count; x++)
+                        {
+                            if (n1 == s[x].name)
+                            {
+                                Console.WriteLine("Success!!!!");
+                                Console.WriteLine("Enter your new username!!!!");
+                                n1 = Console.ReadLine();
+                                s[x].name = n1;
+                                Console.WriteLine("Changed!!!!");
+                                storeDatatoFile(path, s[x].name, s[x].password);
+                            }
+                        }
+                        Console.ReadKey();
+                    }
+                    else if (choice == 4)
+                    {
+                        Console.Clear();
+                        string p1;
+                        Console.WriteLine("Enter  password!!!!");
+                        p1 = Console.ReadLine();
+                        for (int x = 0; x < s.Count; x++)
+                        {
+                            if (p1 == s[x].name)
+                            {
+                                Console.WriteLine("Success!!!!");
+                                Console.WriteLine("Enter your new password!!!!");
+                                p1 = Console.ReadLine();
+                                Console.WriteLine("Changed!!!!");
+                                s[x].password = p1;
+                                storeDatatoFile(path, s[x].name, s[x].password);
+                            }
+                        }
+                        Console.ReadKey();
+                    }
+                    else if (choice == 5)
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                    else
+                    {
+                        Console.Clear();
+                        break;
+                    }
+                } while (true);
+            }
+            else
+            {
+                Console.WriteLine("Error!!!!");
+            }
+            Console.WriteLine("Thanks for using our application!!!!");
+            Console.ReadKey();
+        }
+        static int menu()
+        {
+            int opt;
+            Console.WriteLine("1.SIGN UP!!!!");
+            Console.WriteLine("2.SIGN IN!!!!");
+            Console.WriteLine("3.EXIT!!!!");
+            opt = int.Parse(Console.ReadLine());
+            return opt;
+        }
+        static int userMenuBar()
+        {
+            int opt;
+            Console.WriteLine("1.Add money to account!!!!");
+            Console.WriteLine("2.Withdraw moeny!!!!");
+            Console.WriteLine("3.Change username!!!!");
+            Console.WriteLine("4.Change password!!!!");
+            Console.WriteLine("5.Exit!!!!");
+            opt = int.Parse(Console.ReadLine());
+            return opt;
+        }
+
+        static void signIn(string n, string p, string[] name, string[] password)
+        {
+            bool flage = false;
+            for (int x = 0; x < 8; x++)
+            {
+                if (n == name[x] && p == password[x])
+                {
+                    Console.WriteLine("Valid user!!!!");
+                    flage = true;
+                }
+            }
+            if (flage == false)
+            {
+                Console.WriteLine("Invalid user!!!!");
+            }
+        }
+        static void signUp(string n, string p, string path)
+        {
+            StreamWriter ccc = new StreamWriter(path, true);
+            ccc.WriteLine(n + " " + p);
+            ccc.Flush();
+            ccc.Close();
+        }
+        static string getField(string record, int field)
+        {
+            int comma = 1;
+            string item = " ";
+            for (int x = 0; x < record.Length; x++)
+            {
+                if (record[x] == ',')
+                {
+                    comma++;
+                }
+                else if (comma == field)
+                {
+                    item = item + record[x];
+                }
+
+            }
+            return item;
+        }
+        static bool validuser(string n, string p , List<users> s)
+        {
+            bool flag = false;
+            for(int i =0; i<s.Count;i++)
+            {
+                if(n==s[i].name)
+                {
+                    if (p == s[i].password)
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            return flag;
+        }
+        static void storeDatatoFile(string path, string n , string p)
+        {
+            StreamWriter file = new StreamWriter(path, true);
+            file.WriteLine(n + "," + p);
+            file.Flush();
+            file.Close();
+        }
+        static void loadDataFromFile(string path, List<users> s)
+        {
+            if (File.Exists(path))
+            {
+                StreamReader fileVariable = new StreamReader(path);
+                string record;
+                while ((record = fileVariable.ReadLine()) != null)
+                {
+                    users info = new users();
+                    info.name = getField(record, 1);
+                    info.password = getField(record, 2);
+                    s.Add(info);
+                }
+                fileVariable.Close();
+            }
+            else
+            {
+                Console.WriteLine("Not Exists");
+            }
+        }
+        static void storeUserData(string path1, int add, int deduct)
+        {
+            StreamWriter file = new StreamWriter(path1, false);
+            file.WriteLine(add + "," + deduct);
+            file.Flush();
+            file.Close();
+        }
+        static void loadUserData(string path1, List<functionality> s1)
+        {
+            if (File.Exists(path1))
+            {
+                StreamReader fileVariable = new StreamReader(path1);
+                string record;
+                while ((record = fileVariable.ReadLine()) != null)
+                {
+                    functionality info = new functionality();
+                    info.addedMoney = getField(record, 1);
+                    info.deductedMoney = getField(record, 2);
+                    s1.Add(info);
+                }
+                fileVariable.Close();
+            }
+            else
+            {
+                Console.WriteLine("Not Exists");
+            }
+        }
+     }
+}
---
+++ Week 2 PD/game/Program.cs
@@ -0,0 +1,356 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EZInput;
+
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int plaerHealth = 100;
+            int enemy1Health = 100;
+            int life = 3;
+            int shipX = 32;
+            int shipY = 28;
+            int EnemyX = 42;
+            int EnemyY = 5;
+            int Enemy2X = 35;
+            int Enemy2Y = 7;
+            int[] bulletX = new int[1000];
+            int[] bulletY = new int[1000];
+            bool[] isBulletActive = new bool[1000];
+            int bulletCount = 0;
+            string enemyDirection1 = "Left";
+            char[,] shape = new char[5, 5] {
+    {' ', ' ', 'o', ' ', ' '},
+    {' ', ' ', '|', ' ', ' '},
+    {' ', '-', '-', '-', ' '},
+    {'|', '-', '-', '-', '|'},
+    {'|', '|', ' ', '|', '|'}
+};
+            char [,]enemy1=new char[5,5]
+                {
+   {'|', '|', ' ', '|', '|'},
+    {'|', '-', '-', '-', '|'},
+     {' ', '-', '-', '-', ' '},
+     {' ', ' ', '|', ' ', ' '},
+    {' ', ' ', 'o', ' ', ' '},
+};
+            char[,] enemy2 = new char[5, 5]
+               {
+   {'|', '|', ' ', '|', '|'},
+    {'|', '-', '-', '-', '|'},
+     {' ', '-', '-', '-', ' '},
+     {' ', ' ', '|', ' ', ' '},
+    {' ', ' ', 'o', ' ', ' '},
+};
+  string[,] map = new string[27, 30] {
+          {"_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"||" , " "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," "," ","||"},
+          {"_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_","_"}
+          };
+                int receive;
+                receive=startMenu();
+                if (receive == 1)
+                {
+                printmap();
+                printShip(shipX,shipY,shape);
+                printEnemy1(EnemyX, EnemyY, enemy1);
+                /*printEnemy2(Enemy2X,Enemy2Y,enemy2);*/
+                while (true)
+                {
+                    moveBullet(isBulletActive, bulletX, bulletY, bulletCount);
+                    moveEnemy(EnemyX, EnemyY, enemyDirection1, enemy1);
+                    if (Keyboard.IsKeyPressed(Key.RightArrow))
+                    {
+                        moveShaperight(shipX, shipY, shape);
+                    }
+                    if (Keyboard.IsKeyPressed(Key.LeftArrow))
+                    {
+                        moveShapeleft(shipX, shipY, shape);
+                    }
+                    if (Keyboard.IsKeyPressed(Key.Space))
+                    {
+                        generateBullet(shipX, shipY, shape, bulletX, bulletY, bulletCount, isBulletActive);
+                    }
+                    if(life==0)
+                    {
+                        break;
+                    }
+                    if(plaerHealth==0)
+                    {
+                        life--;
+                        plaerHealth = 100;
+                    }
+                    if(enemy1Health==0)
+                    {
+                        break;
+
+                    }                   
+                }
+                if (receive == 2)
+                {
+
+                }
+                if (receive == 3)
+                {
+
+                }
+            
+            Console.ReadKey();
+        }
+        static int startMenu()
+        {
+            int option;
+            Console.WriteLine("1.Start!!!!");
+            Console.WriteLine("2.Previous record!!!!");
+            Console.WriteLine("3.Exit!!!!");
+            option=int.Parse(Console.ReadLine());
+            return option;
+        }
+        static void printmap()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+             for(int i =0 ; i<27 ; i++)
+            {
+                for(int j =0 ; j <30 ; j++)
+                {
+                    Console.WriteLine(map[i,j]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void printShip(int shipX , int shipY , char[,] shape)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.SetCursorPosition(shipX, shipY);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(shipX, shipY+i);
+                for (int j = 0; j < 5; j++)
+                {
+
+                   Console.Write(shape[i,j]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void printEnemy1(int EnemyX, int EnemyY ,char[,]enemy1)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.SetCursorPosition(EnemyX, EnemyY);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(EnemyX, EnemyY + i);
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write(enemy1[i, j]);
+                }
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+        static void printEnemy2(int Enemy2X, int Enemy2Y, char[,] enemy2)
+        {
+            Console.SetCursorPosition(Enemy2X, Enemy2Y);
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(Enemy2X, Enemy2Y + i);
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write(enemy2[i, j]);
+                }
+            }
+        }
+       static void eraseShape(int shipX , int shipY)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(shipX, shipY + i);
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write(" ");
+                }
+            }
+        }
+        static void erasEnemy1(int EnemyX , int EnemyY)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.SetCursorPosition(EnemyX, EnemyY + i);
+                for (int j = 0; j < 5; j++)
+                {
+
+                    Console.Write(" ");
+                }
+            }
+        }
+        static void moveShaperight(int shipX, int shipY, char[,] shape)
+        {
+            char next = ReadCharFromConsole(shipX + 5, shipY + 4);
+            if (next == ' ')
+            {
+                Console.SetCursorPosition(shipX, shipY);
+                eraseShape(shipX, shipY);
+                shipX++;
+                Console.SetCursorPosition(shipX, shipY);
+                printShip(shipX, shipY, shape);
+            }
+        }
+        static void moveShapeleft(int shipX , int shipY, char[,] shape)
+        {
+            char next = ReadCharFromConsole(shipX - 5, shipY + 4);
+            if (next == ' ')
+            {
+                Console.SetCursorPosition(shipX, shipY);
+                eraseShape(shipX,shipY);
+                shipX--;
+                Console.SetCursorPosition(shipX-1, shipY);
+                printShip(shipX, shipY,shape);
+            }
+        }
+        static void generateBullet(int shipX, int shipY, char[,] shape,int [] bulletX, int[] bulletY,int bulletCount,bool [] isBulletActive)
+        {
+            bulletX[bulletCount] = shipX + 3;
+            bulletY[bulletCount] = shipY - 1;
+            isBulletActive[bulletCount] = true;
+            Console.SetCursorPosition(shipX + 3, shipY - 1);
+            Console.WriteLine( "^");
+            bulletCount++;
+        }
+        static void moveBullet(bool[] isBulletActive, int[] bulletX, int[] bulletY, int bulletCount)
+        {
+            for (int x = 0; x < bulletCount; x++)
+            {
+                if (isBulletActive[x] == true)
+                {
+                    char next = ReadCharFromConsole(bulletX[x], bulletY[x] - 1);
+                    if (next != ' ')
+                    {
+                        eraseBullet(bulletX[x], bulletY[x]);
+                        makeBulletInActive(x, isBulletActive);
+                    }
+                    else if (next == ' ')
+                    {
+                        eraseBullet(bulletX[x], bulletY[x]);
+                        bulletY[x] = bulletY[x] - 1;
+                        printBullet(bulletX[x], bulletY[x]);
+                    }
+                }
+            }
+        }
+        static void printBullet(int x, int y)
+        {
+            Console.SetCursorPosition(x,y);
+            Console.WriteLine("^");
+        }
+        static void eraseBullet(int x, int y)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.WriteLine(" ");
+        }
+        static void makeBulletInActive(int index,bool [] isBulletActive)
+        {
+            isBulletActive[index] = false;
+        }
+        static void moveEnemy(int EnemyX , int EnemyY,string enemyDirection1, char[,] enemy1)
+        {
+            if (enemyDirection1 == "Left")
+            {
+                char next1 = ReadCharFromConsole(EnemyX - 1, EnemyY);
+                char next2 = ReadCharFromConsole(EnemyX - 1, EnemyY + 1);
+                char next3 = ReadCharFromConsole(EnemyX - 1, EnemyY + 2);
+                char next4 = ReadCharFromConsole(EnemyX - 1, EnemyY + 3);
+                char next5 = ReadCharFromConsole(EnemyX - 1, EnemyY + 4);
+                if (next1 == ' ' && next2 == ' ' && next3 == ' ' && next4 == ' ' && next5 == ' ')
+                {
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    erasEnemy1(EnemyX, EnemyY);
+                    EnemyX = EnemyX - 1;
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    printEnemy1(EnemyX, EnemyY, enemy1);
+                }
+                else
+
+                {
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    erasEnemy1(EnemyX, EnemyY);
+                    enemyDirection1 = "Right";
+                }
+            }
+            if (enemyDirection1 == "Right")
+            {
+
+                char next1 = ReadCharFromConsole(EnemyX + 5, EnemyY);
+                char next2 = ReadCharFromConsole(EnemyX + 5, EnemyY + 1);
+                char next3 = ReadCharFromConsole(EnemyX + 5, EnemyY + 2);
+                char next4 = ReadCharFromConsole(EnemyX + 5, EnemyY + 3);
+                char next5 = ReadCharFromConsole(EnemyX + 5, EnemyY + 4);
+                if (next1 == ' ' && next2 == ' ' && next3 == ' ' && next4 == ' ' && next5 == ' ')
+                {
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    erasEnemy1(EnemyX, EnemyY);
+                    EnemyX = EnemyX + 1;
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    printEnemy1(EnemyX, EnemyY, enemy1);
+                }
+                else
+                {
+                    Console.SetCursorPosition(EnemyX, EnemyY);
+                    erasEnemy1(EnemyX, EnemyY);
+                    enemyDirection1 = "Left";
+                }
+            }
+        }
+        public static char ReadCharFromConsole(int x, int y)
+        {
+            // Save the current cursor position
+            int origX = Console.CursorLeft;
+            int origY = Console.CursorTop;
+
+            // Set the cursor position to the desired location
+            Console.SetCursorPosition(x, y);
+
+            // Read the character at the current cursor position
+            ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+
+            // Get the character from the ConsoleKeyInfo object
+            char c = keyInfo.KeyChar;
+            // Restore the original cursor position
+            Console.SetCursorPosition(origX, origY);
+            // Return the character
+            return c;
+        }
+    }
+}
+
